d4196281897d3d447729cb0240edea75e3bef714
Altemar Melendez <altemarmelendez@gmail.com>
2019-03-10 22:36:38 -0400

    Creating new file for routers

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const Post = require('./model/post');
 const mongoose = require('mongoose');
+const postsRoutes = require('./routes/posts');
 
 mongoose.connect('mongodb+srv://alte:A123456@cluster0-92q9t.mongodb.net/node-angular?retryWrites=true', { useNewUrlParser: true })
 .then(() => {
@@ -24,60 +24,7 @@ app.use((req, res, next)=> {
   next();
 });
 
+app.use("/api/posts", postsRoutes);
 // A123456
 
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(postResult => {
-    const postId = postResult.id;
-    res.status(201).json({
-      message: 'Post added Successfully',
-      id: postId
-    });
-  });
-});
-
-app.get('/api/posts', (req, res, next) => {
-Post.find().then((documents) => {
-  console.log(documents);
-  res.status(200).json({
-      message: 'Posts fetched succesfully',
-      posts: documents
-    });
-  });
-});
-
-app.put('/api/posts/:id', (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({_id: req.params.id}, post).then(result => {
-    console.log(result);
-    res.status(200).json({message: 'Update successful!'});
-  });
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    }else {
-      res.status(404).json({ message: "Post not found!"});
-    }
-  });
-});
-
-app.delete('/api/posts/:id', (req, res, next) => {
-  Post.deleteOne({_id: req.params.id}).then(results => {
-    console.log(results);
-    res.status(200).json({
-      message: 'Post deleted!'
-    });
-  });
-});
 module.exports = app;
